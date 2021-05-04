d88a9d3ca53ad83f7aaa4a36dd4740138d70a569
GHB Software MacBook Pro <ghbsoftware@GHBs-MacBook-Pro.local>
2021-05-04 22:17:48 +0300

    Removed hello world classes. Added Kafka and Zookeeper

--- src/test/java/com/pplflw/peopleflow/PeopleflowApplicationTests.java
+++ src/test/java/com/pplflw/peopleflow/PeopleflowApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class PeopleflowApplicationTests {
 
 	@Test
+	@SuppressWarnings("java:S2699") // no assertions in test
 	void contextLoads() {
 	}
 
